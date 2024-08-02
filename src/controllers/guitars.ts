be08c070ec383ddefce7ec763f9437986f4e163d
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2024-08-01 22:02:16 -0400

    Add more controller actions

--- src/controllers/guitars.ts
+++ src/controllers/guitars.ts
@@ -4,13 +4,38 @@ interface ReqRes {
   (req: Request, res: Response): void
 }
 
+interface Guitar {
+  type: string,
+  maker: string,
+  color: string,
+  id: number
+}
+
+const guitars: Guitar[] = []
+
 // CREATE
 
 export const postGuitar: ReqRes = (req, res) => {
+  guitars.push({
+    type: req.body.type,
+    maker: req.body.maker,
+    color: req.body.color,
+    id: req.body.id,
+  })
   res.status(201).send("Made a new guitar!")
 }
 
 // READ
+export const getGuitars: ReqRes = (req, res) => {
+  res.status(200).send(guitars)
+}
+
+export const getGuitar: ReqRes = (req, res) => {
+  const guitar = guitars.find(
+    g => "" + g.id === req.params.id
+  )
+  res.status(200).send(guitar)
+}
 
 // UPDATE
 
--- src/routes/guitars.ts
+++ src/routes/guitars.ts
@@ -1,7 +1,15 @@
 import { Router } from "express"
-import { postGuitar } from "../controllers/guitars"
+import {
+  getGuitar,
+  getGuitars,
+  postGuitar
+} from "../controllers/guitars"
 
 const router = Router()
+
+router.get("/", getGuitars)
+router.get("/:id", getGuitar)
+
 router.post("/", postGuitar)
 
 export default router
